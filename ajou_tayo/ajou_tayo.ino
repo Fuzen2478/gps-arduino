a0c0739e3e9fb50b7c1a8a48b6a9f8ca0adcec70
Fuzen2478 <fuzen2478@gmail.com>
2023-04-17 06:06:10 +0900

    Update ajou_tayo.ino

--- ajou_tayo/ajou_tayo.ino
+++ ajou_tayo/ajou_tayo.ino
@@ -1,68 +1,54 @@
 #include "Arduino.h"
 
-#define RESP_OK                            "OK\r\n"
-#define RET_OK                             1
-#define RET_NOK                            -1
-#define ON                                 1
-#define OFF                                0
-
-#define BG96_PARSER_DELIMITER              "\r\n"
-
 #define MQTT_EOF                           0x1A
+#define message  "+QGPSLOC: 204801.500,37.28331,127.04524,2.0,66.0,2,0.00,0.0,"
 
 String bg96_rsp = "";
-char gps[100];
-char* a = "";
+char gps[500];
 
 void setup() {
+  bg96_rsp = "";
+
   LM5_off();
   LM5_on();
 
-  Serial1.begin(115200); //Cat.M1 Serial
-  while(!Serial1);
+  // Serial.begin(115200);
 
-  while(!WaitCatM1()); //Wait Cat.M1 Ready
-  
-  while(!Setting("ATE0\r", "OK")); //Set Echo Off
-  Pub("ATE0 OK");
+  Serial3.begin(115200); //Arduino Serial
+  while(!Serial3);
+  Serial3.setTimeout(4);
 
-  while(!Setting("AT+QGPS=2\r", "OK")); //Set GPS On
-  Pub("SET GPS OK");
+  delay(6000);
+  Serial3.println("ATE0\r");
+  delay(500);
+  Serial3.println("AT+QGPS=1\r");
+  delay(500);
+  Serial3.println("AT+QIACT=1\r");
+  delay(500);
+  Serial3.println("AT+QMTOPEN=0,\"ficons.iptime.org\",1883\r");
+  delay(1000);
+  Serial3.println("AT+QMTCONN=0,\"fuzen\"\r");
+  delay(500);
 
-  while(!Setting("AT+QIACT=1\r", "OK")); //Set PDP Context on
-  Pub("SET PDP Context OK");
-
-  while(!Setting("AT+QMTOPEN=0,\"ficons.iptime.org\",1883\r", "+QMTOPEN: 0,0"));
-  Pub("MQTT Socket Open OK");
-  //Set MQTT Socket Open
-
-  while(!Setting("AT+QMTCONN=0,\"fuzen\"\r", "+QMTCONN: 0,0,0"));
-  Pub("MQTT Socket Connect OK");
-  //Set MQTT Socket Connect
+  delay(1000);
 }
 
 
 void loop() {
-  Serial1.println("AT+QGPSLOC=2\r");  //Get Gps Location
-  delay(2000);
-  while(Serial1.available()){
-    bg96_rsp += Serial1.ReadStringUntil("\n");
-    bg96_rsp.trim();
-    delay(2);
+  Serial3.println("AT+QGPSLOC=2\r");
+  while(Serial3.available()){
+    bg96_rsp = Serial3.readString();
+    int start = bg96_rsp.indexOf("+QGPSLOC");
+    bg96_rsp.toCharArray(gps, bg96_rsp.length());
+    // Serial.println(gps);
+    if(start > -1){
+      Pub(gps);
+      break;
+    }
+    Serial3.flush();
   }
-  if(bg96_rsp.indexOf("+C") > -1){
-    Pub("Not fixed");
-  }else if(bg96_rsp.indexOf("+QGPSLOC") > 0){ //Need to Update
-    bg96_rsp.toCharArray(a, bg96_rsp.length());
-    Pub(a);
-  }else{
-    bg96_rsp.toCharArray(a, bg96_rsp.length());
-    Pub("Test");
-    Pub(a);
-  }
-  delay(8000);
   bg96_rsp = "";
-  *a = "";
+  delay(5000);
 }
 
 void LM5_off(){
@@ -94,42 +80,8 @@ void LM5_on(){
 }
 
 void Pub(char* payload){
-  Serial1.println("AT+QMTPUB=0,0,0,0,\"gps\"\r");
-  delay(100);
-  Serial1.println(payload);
-  Serial1.write(MQTT_EOF);
-  Serial1.write("\r\n");
-
-  Serial1.ReadStringUntil("\n");  //flush OK
-  Serial1.ReadStringUntil("\n");  //flush \n
-  Serial1.ReadStringUntil("\n");  //flush +QMTPUB: 0,0,0
-  Serial1.ReadStringUntil("\n");  //flush \n
+  Serial3.print("AT+QMTPUB=0,0,0,0,\"gps\"");
+  Serial3.write("\r\n");
+  Serial3.print(payload);
+  Serial3.write(MQTT_EOF);
 }
-
-bool WaitCatM1(){
-  while(Serial1.available()){
-      bg96_rsp += char(Serial1.read());
-      delay(2);
-    }
-    if(bg96_rsp.indexOf("SMS DONE") > 0){
-      Serial.println("Cat.M1 Ready");
-      Serial1.flush();
-      bg96_rsp = "";
-      return true;
-    }
-}
-
-bool Setting(char* payload, char* condition){
-  Serial1.println(payload);
-    delay(100);
-    while(Serial1.available()){
-      bg96_rsp += char(Serial1.read());
-      delay(2);
-    }
-    if(bg96_rsp.indexOf(condition) > 0){
-      Serial1.flush();
-      bg96_rsp = "";
-      return true;
-    }
-}
-
