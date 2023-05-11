ab82d50ce8688bc11cfeeeb8c24fedaacf86a8b9
Fuzen2478 <fuzen2478@gmail.com>
2023-05-12 07:49:42 +0900

    CodeZoo module

--- ajou_tayo/ajou_tayo.ino
+++ ajou_tayo/ajou_tayo.ino
@@ -1,142 +1,119 @@
-#include "Arduino.h"
+#include <TinyGPS.h>
 #include <UnixTime.h>
+#include "TYPE1SC.h"
+#if !defined(__AVR_ATmega4809__)
+#elif !defined(__AVR_ATmega2560__)
+#else
+#include <avr/dtostrf.h>
+#endif
 
-#define MQTT_EOF                           0x1A
-#define message  "+QGPSLOC: 204801.500,37.28331,127.04524,2.0,66.0,2,0.00,0.0,"
+#define DebugSerial Serial
+#define M1Serial Serial1
+#define GpsSerial Serial2
 
-String bg96_rsp = "";
-String str1 = "";
-
+TYPE1SC TYPE1SC(M1Serial, DebugSerial);
+TinyGPS gps;
 UnixTime stamp(9);
 
+char IPAddr[32];
+int _PORT = 2478;
+char sckInfo[128];
+char sendBuffer[] = "";
+char recvBuffer[32];
+int recvSize;
+
 void setup() {
-  bg96_rsp = "";
+  // put your setup code here, to run once:
+  M1Serial.begin(115200);
+  DebugSerial.begin(115200);
+  GpsSerial.begin(9600);
 
-  LM5_off();
-  LM5_on();
+  DebugSerial.println("TYPE1SC Module Start!!!");
+  /* Board Reset */
+  TYPE1SC.reset();
 
-  Serial.begin(115200);
+  /* TYPE1SC Module Initialization */
+  if (TYPE1SC.init()) {
+    DebugSerial.println("TYPE1SC Module Error!!!");
+  }
 
-  Serial3.begin(115200); //Arduino Serial
-  while(!Serial3);
+  /* Network Regsistraiton Check */
+  while (TYPE1SC.canConnect() != 0) {
+    DebugSerial.println("Network not Ready !!!");
+    delay(2000);
+  }
 
-  delay(6000);
-  Serial3.println("ATE0\r");
-  delay(500);
-  Serial3.println("AT+QGPS=1\r");
-  delay(500);
-  Serial3.println("AT+QIACT=1\r");
-  delay(500);
-  Serial3.println("AT+QMTOPEN=0,\"-\",1883\r"); //MQTT 오픈
-  // Serial3.println("AT+QIOPEN=1,0,\"TCP\",\"-\",6379\r"); //소켓 오픈
-  delay(3000);
-  Serial3.println("AT+QMTCONN=0,\"fuzen\"\r");
-  delay(1000);
+  DebugSerial.println("TYPE1SC Module Ready!!!");
 
-  flush();
-  Serial.println("trial : 10");
+  /* Enter a DNS address to get an IP address */
+  while (1) {
+
+    if (TYPE1SC.getIPAddr("ficons.iptime.org", IPAddr,
+                          sizeof(IPAddr)) == 0) {
+      DebugSerial.print("IP Address : ");
+      DebugSerial.println(IPAddr);
+      break;
+    } else {
+      DebugSerial.println("IP Address Error!!!");
+    }
+    delay(2000);
+  }
+
+  /* 1 :TCP Socket Create ( 0:UDP, 1:TCP ) */
+  if (TYPE1SC.socketCreate(1, IPAddr, _PORT) == 0)
+    DebugSerial.println("TCP Socket Create!!!");
+
+INFO:
+
+  /* 2 :TCP Socket Activation */
+  if (TYPE1SC.socketActivate() == 0)
+    DebugSerial.println("TCP Socket Activation!!!");
+
+  if (TYPE1SC.socketInfo(sckInfo, sizeof(sckInfo)) == 0) {
+    DebugSerial.print("Socket Info : ");
+    DebugSerial.println(sckInfo);
+
+    if (strcmp(sckInfo, "ACTIVATED")) {
+      delay(3000);
+      goto INFO;
+    }
+  }
+
+  /* 3 :TCP Socket Send Data */
+
+  if (TYPE1SC.socketSend(sendBuffer) == 0) {
+    DebugSerial.print("[Send] >>  ");
+    DebugSerial.println(sendBuffer);
+  } else
+    DebugSerial.println("Send Fail!!!");
+
 }
-
 
 void loop() {
-  Serial3.println("AT+QGPSLOC=2\r");
-  delay(2000);
-  while(Serial3.available()){
-    bg96_rsp += Serial3.readString();
-  }
-  bg96_rsp = "+QGPSLOC: 225722.0,37.37827,127.11290,1.4,88.0,2,143.07,0.0,0.0,230508,08"; //test
-  int start_gps = bg96_rsp.indexOf("+QGPSLOC"); 
-  int start_cme = bg96_rsp.indexOf("+CME");
-  if(start_cme > -1){
-    str1 = bg96_rsp.substring(start_cme, start_cme + 15);
-  }
-  if(start_gps > -1){
-    str1 = "01,";
-    str1 += get_timestamp();
-    //76~81 -> date 22~27 -> utc maybe in real situation...
-    str1 += bg96_rsp.substring(start_gps + 19, start_gps + 37); //lat, lng
-  }
-  Serial.println(str1);
-  Pub();
-  // send();
-  str1, bg96_rsp = "";
-  flush();
-  delay(3000);
+  delay(5000);
+  strcpy(sendBuffer, getgps(gps).c_str());
+  if (TYPE1SC.socketSend(sendBuffer) == 0) {
+    DebugSerial.print("[Send] >>  ");
+    DebugSerial.println(sendBuffer);
+  } else
+    DebugSerial.println("Send Fail!!!");
 }
 
-void LM5_off(){
-  analogWrite(11, 0);
-  delay(700);
+String getgps(TinyGPS &gps){
+  float lat, lng;
+  int _year, _month, _day, _hour, _minute, _second, _tmp;
 
-  analogWrite(11, 255);
-  delay(2100);
+  gps.f_get_position(&lat, &lng);
 
-  analogWrite(9, 0);
-  //turn off LM5
+  char szTime[32];
+  if (TYPE1SC.getCCLK(szTime, sizeof(szTime)) == 0) {
+    sscanf(szTime,"\"%d/%d/%d,%d:%d:%d+%d\"",&_year, &_month, &_day, &_hour, &_minute, &_second, &_tmp);
+  }    
+  //Set Date Time
+  _year += 2000;
+  stamp.setDateTime(_year, _month, _day, _hour, _minute, _second);   // year/month/day,hour:minute:second
 
-  delay(10000);
-}
-
-void LM5_on(){
-  delay(50);
-
-  analogWrite(11, 0);
-  analogWrite(9, 0);
-  delay(110);
-
-  analogWrite(11, 255);
-
-  delay(4800);
-
-  analogWrite(9, 255);
-  //turn on LM5
-}
-
-void Pub(){
-  Serial3.print("AT+QMTPUB=0,0,0,0,\"gps\"");
-  Serial3.write("\r\n");
-  delay(1000);
-  Serial3.println(str1);
-  Serial3.write(MQTT_EOF);
-}
-
-void send(){
-  Serial3.print("AT+QISEND=0,30");
-  Serial3.write("\r\n");
-  delay(1000);
-  Serial3.print("set bus01 ");
-  Serial3.print(str1);
-  Serial3.write("\r\n");
-}
-
-String get_timestamp(){
-  flush();
-
-  String clk_rsp = "";
-  int year, month, day, hour, minute, second, tmp = 0;
-
-  Serial3.print("AT+CCLK?");
-  Serial3.write("\r\n");
-  delay(500);
-  while(Serial3.available()){
-    clk_rsp += Serial3.readString();
-  }
-  int start_clk = clk_rsp.indexOf("+CCLK");
-  if(start_clk > -1){
-    clk_rsp = clk_rsp.substring(start_clk+7, clk_rsp.length());
-    sscanf(clk_rsp.c_str(),"\"%d/%d/%d,%d:%d:%d+%d\"",&year, &month, &day, &hour, &minute, &second, &tmp);
-  }
-  year += 2000;
-  stamp.setDateTime(year, month, day, hour, minute, second);
-
-  uint32_t unix = stamp.getUnix() + 32400;
-
-  return String(unix) + ",";
-}
-
-void flush(){ //flush Serial3 buffer
-  while(Serial3.available()){
-    Serial3.read();
-  }
-  delay(500);
+  //Get Unix Time 
+  uint32_t unix = stamp.getUnix();
+  return "04," + String(unix) + "," + String(lat) + "," + String(lng);
 }
